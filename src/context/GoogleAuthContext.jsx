54cb850bd127da1931971a17321951f9b2031eec
Ezhilsivaraj <112194071+ezhil-003@users.noreply.github.com>
2024-04-22 00:20:39 +0530

    remove secret

--- src/context/GoogleAuthContext.jsx
+++ src/context/GoogleAuthContext.jsx
@@ -9,8 +9,8 @@ export const useGoogleAuthContext = () => useContext(GoogleAuthContext);
 // Provider component to supply Google OAuth credentials
 export const GoogleAuthContextProvider = ({ children }) => {
     // Replace these with your actual client ID and client secret
-    const clientId = "186201352457-g47v0mr90hbcap0a3lrgsd0o5oupvb17.apps.googleusercontent.com";
-    const clientSecret = "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";
+    const clientId = "use your client id";
+    const clientSecret = "use your client secret";
 
     return (
         <GoogleAuthContext.Provider value={{ clientId, clientSecret }}>
